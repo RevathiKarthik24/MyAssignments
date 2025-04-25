0f4439e2da74814ee2f75c6bb1d9eba207b316e5
RevathiKarthik24 <revathikarthik241092@gmail.com>
2025-04-25 14:41:54 +0530

    Add files via upload
    Day1

---
+++ day1/MyFirstJavaProgram.java
@@ -0,0 +1,20 @@
+package week1.day1;
+
+public class MyFirstJavaProgram {
+
+	// main method
+	public static void main(String[] args) {
+		//console model: ip/op
+		//Input Stream/Output Stream
+		
+		//ctrl+space->content assist for windows
+		//""(double quotes)-->String
+		//''(Sigle quotes)-->char
+		//Not using any quotes-->Integer
+		
+		System.out.println("Revathi Karthik");
+		System.out.println('K');
+		System.out.println(100);
+	}
+
+}
